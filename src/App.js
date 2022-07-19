e189a152baf6da0f38c6744e73c4910d6c43f6e7
Aayush Gupta <aayushgupta.dev@gmail.com>
2022-07-19 16:07:20 +0530

    Changed App.JS

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn AWS alongwith ReactJS!!!
         </a>
       </header>
     </div>
